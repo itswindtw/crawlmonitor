5b87d116423eefe27d54b2546b302f26bc083e0b
Meng-Hsin Tung <itswindtw@gmail.com>
2015-03-31 12:29:38 -0400

    Prepare for deploying

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,8 @@
 # Database
 require 'yaml'
 require 'sequel'
-db_config = YAML.load_file(File.join(BASE_PATH, 'config/database.yml'))[RACK_ENV]
+db_config = ENV['DATABASE_URL']
+db_config ||= YAML.load_file(File.join(BASE_PATH, 'config/database.yml'))[RACK_ENV]
 DB = Sequel.connect(db_config)
 
 Sequel.extension :migration
